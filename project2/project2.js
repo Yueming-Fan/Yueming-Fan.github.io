d05b42f7232c3eedf2106b4a1563de53357d4e07
yueming-fan <yfan010@uottawa.ca>
2022-06-30 08:02:52 -0300

    project2-step3

--- project2/project2.js
+++
@@ -1,9 +0,0 @@
-var name = document.getElementById("name");
-var gender = document.getElementById("gender");
-var age = document.getElementById("age");
-function checkAge() {
-	if (parseInt(age)>=19) {
-		window.location.href="lesson2-1.html";
-	}
-	window.location.href="lesson2-2.html";
-}
